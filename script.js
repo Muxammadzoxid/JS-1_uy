f871c678e9b437e54b8e279bda111d5bf16f8695
Muxammad <markeloffnavi21@gmail.com>
2022-04-30 22:22:17 +0500

    master013

---
+++ script.js
@@ -0,0 +1,78 @@
+/* 
+
+string - malumot turi , u'' yoki "" blan yoziladi
+
+*/
+
+// var name = 'Muxammad'
+// var number = 3.14
+
+// var userName = 'user'
+// var name = 'Muxammad'
+// var age = 16
+
+// console.info('ismim ' +  name + ' yoshim ' + age)/* konketenatsiya */
+
+
+// var plus = 6 + 3
+// var minus = 8 - 2
+// var kop = 5 * 6
+// var bol = 8 / 2
+// var qoldiq = 10 % 3
+// var daraja = 3**3
+
+
+// console.info(plus, minus, kop, bol, qoldiq, daraja)
+
+// console.info('6 + 3 = ' + (6 + 3) )
+// console.info('6 - 3 = ' + (6 - 3))
+// console.info('6 * 3 = ' + 6 * 3)
+// console.info('6 / 3 = ' + 6/ 3)
+// console.info('6 % 3 = ' + 6 % 3)
+
+// var str = prompt('Ismingizni kiritting')
+// alert('Salom ' + str)
+// console.info(str)
+
+// var num = +prompt('son kirit')
+// alert(num)
+// console.info(num)
+
+
+var str = prompt('Ismingizni kiriting' )
+
+var num = prompt('Yowizi kiritn')
+alert('Sizning ismingiz ' + str + ' yowiz ' + num)
+
+
+var str2 = prompt('misol yecamizmi')
+alert('sizi jovobiz ' +  str)
+
+ 
+var num1 = prompt('8 + 2 =')
+var num2 = prompt('8 - 2 =')
+var num3 = prompt('8 * 2 =')
+var num4 = prompt('8 / 2 =')
+
+
+console.info( 'Sizning ismingiz ' + str)
+console.info('Sizning yowingz ' + num)
+
+console.info('sizning jovob 8 + 2 = ' + num1 + (' togri jovob ' + '10 '))
+console.info('sizning jovob 8 - 2 = ' + num2 + (' togri jovob ' + '6 '))
+console.info('sizning jovob 8 * 2 = ' + num3 + (' togri jovob ' + '16 '))
+console.info('sizning jovob 8 / 2 = ' + num4 + (' togri jovob ' + '4 '))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
